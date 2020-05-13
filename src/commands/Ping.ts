7b0f052ff8e79cbd9e1b53e1ba1134ffc1638d61
Kovacs Alex <alex@atm.moe>
2020-05-14 01:48:16 +0300

    fix(commands): import things correctly and remove test.ts

--- src/commands/Ping.ts
+++ src/commands/Ping.ts
@@ -1,6 +1,6 @@
-import Command from '../lib/structures/Command';
-import MoonlightClient from '../lib/Client';
-import BaseManager from '../lib/structures/Managers/Base/BaseManager';
+import { Command } from '../lib/structures/Command';
+import { MoonlightClient } from '../lib/Client';
+import { MoonlightBaseManager as BaseManager } from '../lib/structures/Managers/Base/BaseManager';
 import { Message } from 'discord.js';
 
 export default class extends Command {
