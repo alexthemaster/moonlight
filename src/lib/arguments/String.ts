67c05ab550e9c51de7c3c854be6f1484b1aff528
Kovacs Alex <alex@atm.moe>
2020-07-26 18:27:15 +0300

    chore(): remove unnecesarry line in the string argument

--- src/lib/arguments/String.ts
+++ src/lib/arguments/String.ts
@@ -1,5 +1,4 @@
 export function string(input: string) {
-    if (!input.length) throw 'Please provide an argument of the string type!';
     if (typeof input !== 'string') throw 'The provided argument should be a string!';
     else return input;
 }
