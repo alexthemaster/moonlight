40afafccf5764e9c221ad644cd089cd9f01fbdcd
Penfoldium <crumbs@penfoldium.org>
2020-07-31 21:22:09 +0000

    build(): 3ff0fb6a611f1371bfec15158b8307931968dcfd

--- dist/commands/Enable.js
+++ dist/commands/Enable.js
@@ -4,13 +4,15 @@ const Command_1 = require("../lib/structures/Command");
 /** @ignore */
 class default_1 extends Command_1.Command {
     constructor(client, pool) {
-        super(client, pool, { cooldown: 5, ownerOnly: true, usage: '<piece:string>' });
+        super(client, pool, { ownerOnly: true, usage: '<piece:string>' });
         this.customizeResponse('piece', 'Please provide a piece name to enable!');
     }
     async run(message, args) {
         if (!this.client.pools.has(args.piece))
             return message.channel.send(`No such piece named \`${args.piece}\``);
         const piece = this.client.pools.get(args.piece);
+        if (!(piece === null || piece === void 0 ? void 0 : piece.disabled))
+            return message.channel.send(`This piece is already enabled!`);
         piece.enable();
         return message.channel.send(`Successfully enabled the \`${args.piece}\` piece!`);
     }
--- dist/commands/Reload.js
+++ dist/commands/Reload.js
@@ -5,7 +5,7 @@ const util_1 = require("../lib/util");
 /** @ignore */
 class default_1 extends Command_1.Command {
     constructor(client, pool) {
-        super(client, pool, { cooldown: 5, ownerOnly: true, usage: '[everything] [piece:string]' });
+        super(client, pool, { ownerOnly: true, usage: '[everything] [piece:string]' });
     }
     async run(message, args) {
         if (!args.everything && !args.piece)
