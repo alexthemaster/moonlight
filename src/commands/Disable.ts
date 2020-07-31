bc0c82f59f72d1ee154c9c701b72df24114e07c4
Kovacs Alex <alex@atm.moe>
2020-08-01 00:09:28 +0300

    feat(Commands): add an enable and a disable command

---
+++ src/commands/Disable.ts
@@ -0,0 +1,25 @@
+import { Command } from '../lib/structures/Command';
+import { MoonlightClient } from '../lib/Client';
+import { BasePool } from '../lib/structures/Pools/Base/BasePool';
+import { Message } from 'discord.js';
+
+/** @ignore */
+export default class extends Command {
+    constructor(client: MoonlightClient, pool: BasePool<string, Command>) {
+        super(client, pool, { cooldown: 5, ownerOnly: true, usage: '<piece:string>' });
+        this.customizeResponse('piece', 'Please provide a piece name to enable!')
+    }
+
+    public async run(message: Message, args: DisableCommandArgs) {
+        if (!this.client.pools.has(args.piece)) return message.channel.send(`No such piece named \`${args.piece}\``);
+
+        const piece = this.client.pools.get(args.piece);
+        piece!.disable();
+
+        return message.channel.send(`Successfully disabled the \`${args.piece}\` piece!`);
+    }
+}
+
+interface DisableCommandArgs {
+    piece: string;
+}
