bdf38134600e1877f665904faad0ec9f23f3f2c3
Kovacs Alex <alex@atm.moe>
2020-08-01 00:09:12 +0300

    fix(BasePiece): enabling and disabling wasn't done properly

--- src/lib/structures/BasePiece.ts
+++ src/lib/structures/BasePiece.ts
@@ -28,11 +28,11 @@ export class BasePiece<T> {
     public reload() { }
 
     public enable(): void {
-        if (!this.disabled) this.disabled = false;
+        if (this.disabled) this.disabled = false;
     }
 
     public disable(): void {
-        if (!!this.disabled) this.disabled = true;
+        if (!this.disabled) this.disabled = true;
     }
 
     constructor(client: MoonlightClient, pool: BasePool<string, T>, options?: BasePieceOptions) {
