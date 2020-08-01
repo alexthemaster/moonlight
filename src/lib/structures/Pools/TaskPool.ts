5cb61c4ddfbf6d05a00c56c3a9a18fb9f1618d70
Kovacs Alex <alex@atm.moe>
2020-08-01 15:26:39 +0300

    fix: this too

--- src/lib/structures/Pools/TaskPool.ts
+++ src/lib/structures/Pools/TaskPool.ts
@@ -4,6 +4,6 @@ import { Task } from '../Task';
 
 export class TaskPool<K, V> extends BasePool<K, V> {
     constructor(client: MoonlightClient) {
-        super(client, 'events', Task);
+        super(client, 'task', Task);
     }
 }
