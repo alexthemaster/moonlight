e300bc173f3c6d535a9b207fd6d493aaa215e098
Kovacs Alex <alex@atm.moe>
2020-08-16 21:04:02 +0300

    chore: move functions around and some more typing

--- src/lib/util/Paginator.ts
+++ src/lib/util/Paginator.ts
@@ -30,14 +30,8 @@ export class Paginator {
         if (navigation) this._navigation = navigation;
     }
 
-    /** Add a page to the embed */
-    public add(embedFunction: (embed: MessageEmbed) => MessageEmbed): this {
-        this.pages.push(embedFunction(new MessageEmbed(this.templateEmbed)));
-        return this;
-    }
-
     /** Start the paginator */
-    public async start() {
+    public async start(): Promise<void> {
         // Send a message with the default page embed
         const message = await this._message.channel.send(this.pages[this.currentPage - 1].setFooter(this.footer));
         // React to the sent message with all the navigation emojis
@@ -55,70 +49,79 @@ export class Paginator {
         })
     }
 
-    private async _updatePage(message: Message) {
+    /** Add a page to the embed */
+    public add(embedFunction: (embed: MessageEmbed) => MessageEmbed): this {
+        this.pages.push(embedFunction(new MessageEmbed(this.templateEmbed)));
+        return this;
+    }
+
+    /** Set a custom footer to use with the default embed template */
+    public setCustomFooter(footer: string): void {
+        this._customFooter = footer;
+    }
+
+    private async _updatePage(message: Message): Promise<this> {
         await message.edit(this.pages[this.currentPage - 1].setFooter(this.footer));
         return this;
     }
 
-    private async _removeReactions(message: Message) {
-        // We get the IDs of all users from the cache and create a Set then convert it back to an array to remove duplicates
-        const users = [...new Set(message.reactions.cache.map(reaction => reaction.users.cache.filter(user => user !== message.author).map(user => user.id)).flat(undefined))];
-        await Promise.all(users.map(user => message.reactions.cache.map(reaction => reaction.users.remove(user as unknown as User))));
-    }
-
-    public get footer(): string {
-        return this._customFooter ? `${this._customFooter} | ${this.currentPage}/${this.pages.length}` : `${this.currentPage}/${this.pages.length}`;
-    }
-
-    /** Set a custom footer to use with the default embed template */
-    public setCustomFooter(footer: string) {
-        this._customFooter = footer;
-    }
-
-    private async _askPage() {
+    private async _askPage(): Promise<number> {
         const tmp = await this._message.channel.send(`What page would you like to jump to?`);
         const tmpPage = await this._message.channel.awaitMessages((message: Message) => message.author === this._message.author && !isNaN(Number(message.content)), { max: 1 });
         await tmp.delete();
         return Number(tmpPage.first()?.content)
     }
 
+    private async _removeReactions(message: Message): Promise<void> {
+        // We get the IDs of all users from the cache and create a Set then convert it back to an array to remove duplicates
+        const users = [...new Set(message.reactions.cache.map(reaction => reaction.users.cache.filter(user => user !== message.author).map(user => user.id)).flat(undefined))];
+        await Promise.all(users.map(user => message.reactions.cache.map(reaction => reaction.users.remove(user as unknown as User))));
+    }
+
     /** Set the page  paginator is on */
-    public async page() {
+    public async page(): Promise<this> {
         const page = await this._askPage();
-        if (page > this.pages.length) return await this._message.channel.send(`This page number is too big, please retry. (max page number: ${this.pages.length})`);
+        if (page > this.pages.length) {
+            await this._message.channel.send(`This page number is too big, please retry. (max page number: ${this.pages.length})`);
+            return this;
+        }
 
         this.currentPage = page;
         return this;
     }
 
     /** Go to the next page */
-    public async forward() {
+    public async forward(): Promise<this> {
         if (this.currentPage < this.pages.length) this.currentPage += 1;
         return this;
     }
 
     /** Go to the previous page */
-    public async backward() {
+    public async backward(): Promise<this> {
         if (this.currentPage >= this.pages.length) this.currentPage -= 1;
         return this;
     }
 
     /** Jump to the first page */
-    public async initial_page() {
+    public async initial_page(): Promise<this> {
         this.currentPage = 1;
         return this;
     }
 
     /** Jump to the last page */
-    public async last_page() {
+    public async last_page(): Promise<this> {
         this.currentPage = this.pages.length;
         return this;
     }
 
     /** Stop the paginator */
-    public async stop() {
+    public async stop(): Promise<void> {
         await this._collector?.message.reactions.removeAll();
         this._collector?.stop();
+    }
+
+    public get footer(): string {
+        return this._customFooter ? `${this._customFooter} | ${this.currentPage}/${this.pages.length}` : `${this.currentPage}/${this.pages.length}`;
     }
 }
 
